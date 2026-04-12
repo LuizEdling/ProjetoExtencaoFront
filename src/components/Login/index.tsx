64f26027d4c8a0a44588902e3038b33a0f17cfe4
Danton <dantyt337@gmail.com>
2026-04-12 16:16:09 -0300

    F_LOGIN = alterando colunas da tabela de usuários e adicionando a criação dos dados para os mesmos, como email e password padrões que vão ser alterados posteriormente nas seeds

---
+++ src/components/Login/index.tsx
@@ -0,0 +1,13 @@
+import DesktopLoginForm from "./DesktopLoginForm";
+import { useIsDesktop } from "../../hooks/useIsDesktop";
+import MobileLoginForm from "./MobileLoginForm";
+
+export default function Login(){
+  const isDesktop = useIsDesktop();
+
+  return(
+    <div className="bg-(--background-first-layer)">
+      {isDesktop ? <MobileLoginForm/> : <DesktopLoginForm/>}
+    </div>
+  );
+}
